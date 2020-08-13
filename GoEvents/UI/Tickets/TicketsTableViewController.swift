c14c63ee53a16aadbe173606eeed825bced22a82
Brenner <savyobrenner@gmailcom>
2020-08-12 21:41:06 -0300

    add ticketTable Screen

---
+++ GoEvents/UI/Tickets/TicketsTableViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class TicketsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+
+        return cell
+    }
+
+}
